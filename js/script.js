be0a60e92b862dd6d548be3623503ae568866bd4
Wyuichiw <yuichiwaite2206@gmail.com>
2026-03-12 15:21:49 +0200

    Remove comments from script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Знаходимо картинку котика на сторінці
+
 const catImage = document.querySelector('img');
 
-// Додаємо "слухача", який чекає на клік мишкою по картинці
+
 catImage.addEventListener('click', function() {
     alert('Мяу! 😎 Ти погладив найкрутішого котика!');
 });
